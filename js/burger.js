78031fe6a1efbf1e1712c113b8168f7598c5c151
Jack <verumjack@gmail.com>
2023-03-14 12:15:48 +0200

    Edit path for fonts. Edit max-width to 1040px. Delete comment JS files and path

--- js/burger.js
+++ js/burger.js
@@ -1,13 +1,3 @@
-// $(document).ready(function() {
-//     $('.header__wrapper__burger').click(function(event) {
-//         $('.header__wrapper__burger,.header__wrapper__menu').toggleClass('active');
-//         $('body').toggleClass('lock');
-//     })
-// })
-
-// console.log(555);
-
-
 let burger = document.querySelector('.header__wrapper__burger');
 let burgerMenu = document.querySelector('.header__wrapper__menu');
 
