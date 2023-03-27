7327447428f3ed0a5265beb738f1ebb4accbe757
verumjack <verumjack@gmail.com>
2023-03-27 11:52:27 +0300

    Modify Block Study. Delele text-shadow. Add toggle to card. Delete comment

--- js/burger.js
+++ js/burger.js
@@ -5,3 +5,21 @@ burger.addEventListener('click', (event) => {
     burger.classList.toggle('active');
     burgerMenu.classList.toggle('active');
 })
+
+
+
+
+let study = document.querySelectorAll('.study__card');
+
+study.forEach((e) => {
+    e.classList.remove("card-active")
+    e.addEventListener('click', () => {
+        e.classList.toggle('card-active');
+    })
+})
+
+
+
+
+
+
