3d8d86119eb826919ce86bf4d3c111cbdef8d47e
sryb <sryb@tmxpub.agiotg.com>
2023-03-30 05:19:25 +0300

    burgerMenuItems click event menu close

--- js/burger.js
+++ js/burger.js
@@ -1,8 +1,17 @@
 let burger = document.querySelector('.header__wrapper__burger');
 let burgerMenu = document.querySelector('.header__wrapper__menu');
+let burgerMenuItems = document.querySelectorAll('.header__wrapper__list li');
+burgerMenuItems = [...burgerMenuItems];
 
-burger.addEventListener('click', (event) => {
+burger.addEventListener('click', () => {
     burger.classList.toggle('active');
     burgerMenu.classList.toggle('active');
+})
+
+burgerMenuItems.forEach((item) => {
+    item.addEventListener('click', () => {        
+        burgerMenu.classList.remove('active');
+        burger.classList.remove('active');
+    })
 })
 
