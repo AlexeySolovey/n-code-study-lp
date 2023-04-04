4f181bc5602c7d58cade8063f2c4a57f6318ae33
sryb <sryb@tmxpub.agiotg.com>
2023-04-04 11:16:28 +0300

    start new proj. mob-desc header, advantages; without mob menu

--- js/burger.js
+++
@@ -1,7 +0,0 @@
-let burger = document.querySelector('.header__wrapper__burger');
-let burgerMenu = document.querySelector('.header__wrapper__menu');
-
-burger.addEventListener('click', (event) => {
-    burger.classList.toggle('active');
-    burgerMenu.classList.toggle('active');
-})
