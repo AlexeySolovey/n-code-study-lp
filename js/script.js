b883c8072cbaf30b81d8eedb4743484f8c0a9b6c
sryb <sryb@tmxpub.agiotg.com>
2023-04-06 00:27:41 +0300

    fix mob menu; menu icon animation

--- js/script.js
+++ js/script.js
@@ -2,10 +2,7 @@ const hamburger_menu = document.querySelector(".hamburger-menu");
 const mob_menu = document.querySelector(".mob-menu");
 const close_menu = document.querySelector(".menu-close");
 
-hamburger_menu.addEventListener("click", () => {
-    mob_menu.classList.add("active");
-});
-
-close_menu.addEventListener("click", () => {
-    mob_menu.classList.remove("active");
+hamburger_menu.addEventListener("click", function(e){
+    mob_menu.classList.toggle("active");
+    this.classList.toggle("active");
 });
