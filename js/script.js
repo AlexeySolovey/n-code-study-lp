31a910faabf029e9105e4ee6712e72dcab9904d7
sryb <sryb@tmxpub.agiotg.com>
2023-04-08 07:38:34 +0300

    roadmap script

--- js/script.js
+++ js/script.js
@@ -1,8 +1,34 @@
-const hamburger_menu = document.querySelector(".hamburger-menu");
-const mob_menu = document.querySelector(".mob-menu");
-const close_menu = document.querySelector(".menu-close");
+(function mobMenu() {
 
-hamburger_menu.addEventListener("click", function(e){
-    mob_menu.classList.toggle("active");
-    this.classList.toggle("active");
-});
+    const hamburger_menu = document.querySelector(".hamburger-menu");
+    const mob_menu = document.querySelector(".mob-menu");
+
+    hamburger_menu.addEventListener("click", function (e) {
+        mob_menu.classList.toggle("active");
+        this.classList.toggle("active");
+    });
+
+}())
+
+
+(function roadmap() {
+    let roadmapItems = document.querySelector(".roadmap-step");
+    roadmapItems = Array.from(roadmapItems.children);
+
+    let roadmapInfoItems = document.querySelector(".roadmap-step-info");
+    roadmapInfoItems = Array.from(roadmapInfoItems.children);
+
+    roadmapItems.forEach((item, index) => {
+        item.addEventListener('click', function () {
+            roadmapItems.forEach(item => item.classList.remove("active"));
+            roadmapInfoItems.forEach(item => item.classList.remove("active"));
+            roadmapInfoItems[index].classList.add("active");
+            item.classList.add("active");
+        })
+    });
+
+}())
+
+
+
+// roadmap();
