b83eef042e9402f8ba8ebde67d99e25ac6f72a41
sryb <sryb@tmxpub.agiotg.com>
2023-04-18 11:37:38 +0300

    some fix

--- js/script.js
+++ js/script.js
@@ -44,10 +44,10 @@
     })
 }());
 
-(function customSelect() {
+(function customSelect () {
     NiceSelect.bind(document.getElementById("course"));
     NiceSelect.bind(document.getElementById("wherefrom"));
-
+    
     let selectsPlaceholder = document.querySelectorAll('.nice-select .current');
     selectsPlaceholder[0].innerHTML = 'Оберіть напрямок';
     selectsPlaceholder[1].innerHTML = 'Звідки ви дізнались про N-Code?';
@@ -57,7 +57,7 @@
     const form = document.querySelector('#callbackForm');
     const btn = document.querySelector('.callback-form .btn')
 
-    btn.addEventListener('click', function (event) {
+    btn.addEventListener('click', function(event){
         event.preventDefault();
         const inputs = form.querySelectorAll('input');
         inputs.forEach(function (input) {
